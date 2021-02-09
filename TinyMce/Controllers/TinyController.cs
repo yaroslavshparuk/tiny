46f38219b1e4a8b9b77555c434ad4ce8e92fe905
Yaroslav Shparuk <shparuk1996@gmail.com>
2021-02-09 11:56:04 +0200

    init

---
+++ TinyMce/Controllers/TinyController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Http;
+using TinyMce.Models;
+
+namespace TinyMce.Controllers
+{
+    public class TinyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public MemoryStream Index(ExampleClass model)
+        {
+            using (var client = new HttpClient())
+            {
+                var request = new HttpRequestMessage();
+                request.RequestUri = new Uri("http://webapi/pdf/?html=" + model.HtmlContent);
+                var response = client.SendAsync(request).Result;
+                return new MemoryStream(response.Content.ReadAsByteArrayAsync().Result);
+            }
+        }
+    }
+    public class ExampleClass
+    {
+        public string HtmlContent { get; set; }
+
+        public ExampleClass()
+        {
+
+        }
+    }
+}
---
+++ WebApi/Controllers/PdfController.cs
@@ -0,0 +1,32 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.html.simpleparser;
+using iTextSharp.text.pdf;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PdfController : ControllerBase
+    {
+        [HttpGet]
+        [Obsolete]
+        public MemoryStream Get(string html = "<h1>ERROR</h1>")
+        {
+            byte[] bytes;
+            var sr = new StringReader(html);
+            var pdfDoc = new Document(PageSize.A4, 10f, 10f, 10f, 0f);
+            var htmlparser = new HTMLWorker(pdfDoc);
+            using (var memoryStream = new MemoryStream())
+            {
+                var writer = PdfWriter.GetInstance(pdfDoc, memoryStream);
+                pdfDoc.Open();
+                htmlparser.Parse(sr);
+                pdfDoc.Close();
+                bytes = memoryStream.ToArray();
+            }
+            return new MemoryStream(bytes);
+        }
+    }
+}
